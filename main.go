19e336a06519a4680e41f3a7f6d85c0580505825
Igor <igoracmelo@protonmail.com>
2023-04-21 23:21:25 -0300

    rewriting the tokenizer using in/out channels

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 	"unicode"
 )
 
@@ -41,65 +41,105 @@ func (t token) String() string {
 	return fmt.Sprintf("{ %s `%s` }", ttStr[t.typ], t.val)
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		panic("missing math expression")
-	}
-	code := []rune(os.Args[1])
-
-	i := 0
-	for i <= len(code) {
-		t, d := nextToken(code[i:])
-		i += d + 1
-		fmt.Println(t)
-		if t.typ == ttEOF {
-			break
-		}
-	}
+type lexer struct {
+	in      chan rune
+	out     chan token
+	seqStr  string
+	seqType tokenType
 }
 
-func nextToken(rs []rune) (token, int) {
-	var r rune
-	var i int
-
-	for i, r = range rs {
-		if unicode.IsSpace(r) {
-			continue
-		}
-
-		var tt tokenType
-
-		if unicode.IsDigit(r) {
-			tt = ttNum
-			for j := i + 1; j < len(rs); j++ {
-				r := rs[j]
-				if !unicode.IsDigit(r) {
-					break
-				}
-				i++
-			}
-			return token{ttNum, string(rs[:i+1])}, i
-		}
-
-		switch r {
-		case '+':
-			tt = ttAdd
-		case '-':
-			tt = ttSub
-		case '*':
-			tt = ttMul
-		case '/':
-			tt = ttDiv
-		case '(':
-			tt = ttLParen
-		case ')':
-			tt = ttRParen
-		default:
-			tt = ttIllegal
-		}
-
-		return token{tt, string(r)}, i
+func (l *lexer) sendSeq() {
+	if len(l.seqStr) == 0 {
+		return
 	}
 
-	return token{ttEOF, ""}, i
+	l.out <- token{l.seqType, l.seqStr}
+	l.seqStr = ""
+	l.seqType = ttIllegal
+}
+
+func (l *lexer) tokenize() {
+	r, ok := <-l.in
+
+	if !ok {
+		l.sendSeq()
+		l.out <- token{ttEOF, ""}
+		close(l.out)
+		return
+	}
+
+	if unicode.IsSpace(r) {
+		l.sendSeq()
+		l.tokenize()
+		return
+	}
+
+	if unicode.IsDigit(r) {
+		if l.seqType != ttNum {
+			l.sendSeq()
+		}
+		l.seqStr += string(r)
+		l.seqType = ttNum
+		l.tokenize()
+		return
+	}
+
+	l.sendSeq()
+	var tt tokenType
+	switch r {
+	case '+':
+		tt = ttAdd
+	case '-':
+		tt = ttSub
+	case '*':
+		tt = ttMul
+	case '/':
+		tt = ttDiv
+	case '(':
+		tt = ttLParen
+	case ')':
+		tt = ttRParen
+	default:
+		tt = ttIllegal
+	}
+
+	l.out <- token{tt, string(r)}
+
+	if tt == ttIllegal {
+		close(l.out)
+		return
+	}
+
+	l.tokenize()
+}
+
+func main() {
+	if len(os.Args) < 0 {
+		panic("missing math expression")
+	}
+
+	l := lexer{
+		in:  make(chan rune),
+		out: make(chan token),
+	}
+
+	code := os.Args[1]
+
+	go func() {
+		for _, r := range code {
+			l.in <- r
+		}
+		close(l.in)
+	}()
+
+	go func() {
+		l.tokenize()
+	}()
+
+	for t := range l.out {
+		fmt.Println(t)
+		if t.typ == ttIllegal {
+			panic(fmt.Sprintf("illegal character %q", t.val))
+		}
+	}
 }
